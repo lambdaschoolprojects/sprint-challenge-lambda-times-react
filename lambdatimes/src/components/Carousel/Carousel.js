6433f9a3186b51f9b2169cb8b65f59e7c0a645fc
Rane Wallin <RAN2143066@maricopa.edu>
2019-03-22 19:20:43 -0700

    Stretch: Starting to work on carousel. Added carousel data in CDM

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,35 +1,37 @@
-import React, { Component } from 'react';
-import { carouselData } from '../../data'
-// Complete this Carousel 
+import React, { Component } from "react";
+import { carouselData } from "../../data";
+// Complete this Carousel
 export default class Carousel extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-
-    }
+      selectedImage: ""
+    };
   }
-  componentDidMount(){
-
-  }
-
-  leftClick = () => {
-
+  componentDidMount() {
+    this.setState({ data: carouselData });
   }
 
-  rightClick = () => {
+  leftClick = () => {};
 
-  }
+  rightClick = () => {};
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
-  }
-  
-  render(){
+    let { selectedImage } = this.state;
+
+    return <img src={selectedImage} style={{ display: "block" }} />;
+  };
+
+  render() {
     return (
       <div className="carousel">
-        <div className="left-button" onClick={this.leftClick}>{"<"}</div>
-        <div className="right-button" onClick={this.rightClick}>{">"}</div>
+        <div className="left-button" onClick={this.leftClick}>
+          {"<"}
+        </div>
+        <div className="right-button" onClick={this.rightClick}>
+          {">"}
+        </div>
       </div>
-    )
+    );
   }
 }
